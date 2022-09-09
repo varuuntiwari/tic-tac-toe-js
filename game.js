aebeebca50025c5f8d138732452fa59b407980db
Varun <62049199+varuuntiwari@users.noreply.github.com>
2022-09-09 16:29:46 +0530

    First working commit

---
+++ game.js
@@ -0,0 +1,64 @@
+var playerTurn = "x"
+
+function circleCross(e) {
+    // Filling the clicked box
+    if(e.innerHTML === "") {
+        if(playerTurn === "x") {
+            e.innerHTML = "x"
+            playerTurn = "o"
+        } else if (playerTurn === "o") {
+            e.innerHTML = "o"
+            playerTurn = "x"
+        }
+    } else {
+        alert("box already filled")
+    }
+
+    // Checking for winning conditions
+    if(lineMatch("o")) {
+        alert("circle wins")
+    } else if(lineMatch("x")) {
+        alert("cross wins")
+    }
+}
+
+function lineMatch(s) {
+    let boxes = document.querySelectorAll(".box")
+    let pattern = []
+    boxes.forEach((box) => {
+        pattern.push(box.innerText)
+    })
+    // Horizontal match
+    for(let i=0; i < 7; i++) {
+        if(pattern[i] === s && pattern[i+1] === s && pattern[i+2] === s) {
+            return true
+        }
+    }
+    // Vertical match
+    for(let i=0; i < 3; i++) {
+        if(pattern[i] === s && pattern[i+3] === s && pattern[i+6] === s) {
+            return true
+        }
+    }
+    // Diagonal match
+    if((pattern[0] === s && pattern[4] === s && pattern[8] === s) ||
+        (pattern[2] === s && pattern[4] === s && pattern[6] === s)) {
+            return true
+    }
+    // Keeps giving true and alert statement
+    /* if(boxFull()) {
+        alert("all boxes full")
+    } */
+    return false
+}
+
+// Always returns true
+function boxFull() {
+    let boxes = document.querySelectorAll(".box")
+    boxes.forEach((box) => {
+        if(box.innerText === "") {
+            return false
+        }
+    })
+    return true
+}
